7409e875d776a9064d28d7ae6877a52c45bed345
Will-Andrade <willian.santos.andra@outlook.com>
2022-01-23 22:01:18 -0400

    adds basic functionalities and refactoring

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+const textBox = document.querySelector('[data-js="text-box"]');
+const charactersCount = document.querySelector('[data-js="characters-count"]');
+const wordsCount = document.querySelector('[data-js="words-count"]');
+
+const filterWords = word => word !== '';
+
+const countWordsAmount = sentence => 
+    sentence.split(' ').filter(filterWords).length;
+
+const textBoxHandler = ({ target }) => {
+    charactersCount.textContent = target.value.replace(/\s/g, '').length;
+    wordsCount.textContent = countWordsAmount(target.value);
+};
+
+textBox.addEventListener('input', textBoxHandler);
